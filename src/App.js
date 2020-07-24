eacbb67914265855cd23a038e9baa8ecfa3552d5
mohameddaoud99 <mohamed.daoud99@hotmail.com>
2020-07-24 10:18:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [mytask, SetMytask] = useState("")
+    const [tasks, SetTasks] = useState([])
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        SetMytask(e.target.value)
+    }
+    const addTasks = (e) => {
+        e.preventDefault();
+        SetTasks([...tasks, { name: mytask, id: tasks.length }]);
+        SetMytask("")
+
+    };
+
+    const deleteTasks = (id) => {
+        SetTasks(tasks.filter(el => el.id != id))
+    }
+
+
+    const EditTask = (id) => {
+        let x = prompt('give me your new task here')
+        SetTasks(tasks.map(el => (el.id == id) ? { id: id, mytask: x } : el))
+        console.log(x)
+    }
+
+
+
+
+
+    return (
+        <div className="App">
+            <form id="to-do-form" onSubmit={addTasks}>
+                <input placeholder="Enter text" type="text" name="task" value={mytask} onChange={handleChange}></input>
+                <button id="add-button" type="submit">Add task</button>
+            </form>
+
+
+
+
+            <ul className="unlist"> {tasks.map(task => <li className="list" key={task.id}>
+                <span>{task.name}</span>
+                <span>{task.mytask}</span>
+                <button onClick={() => deleteTasks(task.id)}> X </button>
+                <button onClick={() => EditTask(task.id)}>Edit</button>
+
+            </li>
+            )} </ul>
+
+
+
+
+
+
+        </div>
+    );
 }
 
 export default App;
